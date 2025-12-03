f581b9b49e762c8546f631e3902ec6e436ab5453
Vanah-Nss <safidysylvana333@gmail.com>
2025-12-03 09:48:09 +0300

    fix: force new build hash

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,29 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 5173,
     host: true,
   },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
-  },
+
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
+      }
+    }
+  }
 })
+
