79a783b1fe2fbc6f7d5418a88f9af69f9ce0912b
Vanah-Nss <safidysylvana333@gmail.com>
2025-12-03 09:34:40 +0300

    add: config file with API URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-// Configuration de l'API
 export const API_URL = "https://finalproject-bu3e.onrender.com/api/graphql/";
 export const API_BASE_URL = "https://finalproject-bu3e.onrender.com";
-
-console.log("✅ Config chargée - API URL:", API_URL);
+console.log("✅ Config loaded - API URL:", API_URL);
